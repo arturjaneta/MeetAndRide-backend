8de185b6b6e2de076b774afe054283482de9fc04
arturjaneta <32541851+arturjaneta@users.noreply.github.com>
2020-11-02 10:49:47 +0100

    initialization

---
+++ src/main/java/pl/polsl/meetandride/MeetandrideApplication.java
@@ -0,0 +1,13 @@
+package pl.polsl.meetandride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetandrideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetandrideApplication.class, args);
+	}
+
+}
